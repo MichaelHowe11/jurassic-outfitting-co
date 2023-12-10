2d6acd4016f1ef57ef473d6678fb69bcfb6701c9
TahaS-Coding <143848726+TahaS-Coding@users.noreply.github.com>
2023-12-09 23:00:50 -0800

    completed all but packages and categories mobile styles

--- jurassic-outfitting-co/js/faux-cart.js
+++ jurassic-outfitting-co/js/faux-cart.js
@@ -1,6 +1,6 @@
 const
     // set the name of your shop here
-    shopID = 'Jurassic Outfitting Co.',
+    shopID = 'testShop',
     // match the following attributes to the classes on your products
     productClass = 'product',
     imageClass = 'prodImage',
